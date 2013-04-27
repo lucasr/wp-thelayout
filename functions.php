f89b64a701eef1c1e7d753897fb0738ffdfce5fa
Lucas Rocha <lucasr@lucasr.org>
2013-04-28 00:47:54 +0100

    [functions] Uncomment transients bits

--- functions.php
+++ functions.php
@@ -15,9 +15,9 @@ add_action( 'publish_post', 'lucasr_delete_thelayout_on_new_post' );
 
 
 function lucasr_get_thelayout_posts( $future = false ) {
-    // $thelayout_posts = get_transient( $future ? 'thelayout_future_posts' : 'thelayout_posts' );
+    $thelayout_posts = get_transient( $future ? 'thelayout_future_posts' : 'thelayout_posts' );
 
-    // if ( $thelayout_posts === false ) {
+    if ( $thelayout_posts === false ) {
         $thelayout_posts = new WP_Query( array(
             'posts_per_page' => 20,
             'offset' => 0,
@@ -27,8 +27,8 @@ function lucasr_get_thelayout_posts( $future = false ) {
             'orderby' => 'date'
         ) );
 
-        // set_transient( 'thelayout_posts', $thelayout_posts, 24 * HOUR_IN_SECONDS );
-    // }
+        set_transient( 'thelayout_posts', $thelayout_posts, 24 * HOUR_IN_SECONDS );
+    }
 
     return $thelayout_posts;
 }
